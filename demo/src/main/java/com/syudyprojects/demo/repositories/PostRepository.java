fa668a013ace03c7bc6b934ea2e2a51d931acab8
EricklisCruz <erickliscruz@gmail.com>
2023-08-02 10:07:19 -0300

    @Query

--- demo/src/main/java/com/syudyprojects/demo/repositories/PostRepository.java
+++ demo/src/main/java/com/syudyprojects/demo/repositories/PostRepository.java
@@ -2,6 +2,7 @@ package com.syudyprojects.demo.repositories;
 
 import com.syudyprojects.demo.domain.entities.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +10,7 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> findBySearchTitle(String text);
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
